7d3d38a71884b5fedd33dcd208c134b42e18d74b
Gary Ho <garyho0916@gmail.com>
2023-01-08 00:05:14 -0800

    Install react-markdown and styled-components

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,5 +1,24 @@
-export default function BlogPage() {
-    return (
-        <div>Hello</div>
-    )
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm'
+
+export default function Blog() {
+  const markdown = `
+A paragraph with *emphasis* and **strong importance**.
+
+> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+    
+* Lists
+* [ ] todo
+* [x] done
+    
+A table:
+    
+| a | b |
+| - | - |
+`
+  return (
+    <ReactMarkdown className='markdown-body' remarkPlugins={[remarkGfm]}>
+      {markdown}
+    </ReactMarkdown>
+  )
 }
