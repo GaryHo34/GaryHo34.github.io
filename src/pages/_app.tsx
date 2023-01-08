d83d0300be379858be6171a13f80d948f7add3a9
Gary Ho <garyho0916@gmail.com>
2023-01-08 00:02:47 -0800

    Install react-markdown and styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { GlobalStyled } from 'styles/globals';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const defaultTheme = 'light'
+  return (
+    <>
+      <GlobalStyled theme={defaultTheme} />
+      <Component {...pageProps} />
+    </>
+  )
 }
